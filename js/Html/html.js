c416e384ed04841bccea251b0573df02209eab8e
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2026-02-02 10:55:19 +0300

    added some Q&A about html

---
+++ js/Html/html.js
@@ -0,0 +1,12 @@
+/**
+ * html & css 30 interview questions
+ * 1.what is html?
+ * Ansewer: html (hper text markup language) is used to  structure content on the web elements like headings,paragraphs,links,images and forms.
+ * 
+ * 2.what are semantic elements in html?
+ * Answer: semantic elements clearly describe their meaning in a human and machine-readable way.Examples of semantic elements include <header>,<footer>,<article>,<section>,<nav> etc.
+ * 
+ * 
+ * 
+ * 
+ */
